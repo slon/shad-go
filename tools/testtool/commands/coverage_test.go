9848183cc15673f869b9599dd863fbc921d21e33
Arseny Balobanov <verytable@yandex.ru>
2020-03-19 01:40:57 +0300

    Cleanup comments.

--- tools/testtool/commands/coverage_test.go
+++ tools/testtool/commands/coverage_test.go
@@ -10,8 +10,4 @@ func Test_getCoverageRequirements(t *testing.T) {
 	r := getCoverageRequirements("../testdata/coverage/sum")
 	require.True(t, r.Enabled)
 	require.Equal(t, 90.0, r.Percent)
-	t.Logf("r: %+v", r)
-	//require.Equal(t,
-	//	absPaths([]string{"sum/private_test.go", "sum/public_test.go"}),
-	//	getCoverageRequirements("../testdata/coverage"))
 }
