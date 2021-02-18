fc1ac9aa48b43e5a32a62df3b9143379d38a00f1
verytable <verytable@yandex.ru>
2021-02-18 19:47:04 +0000

    Resolve "add text/template task"

---
+++ ciletters/notification.go
@@ -0,0 +1,43 @@
+// +build !change
+
+package ciletters
+
+type Notification struct {
+	Project  GitlabProject
+	Branch   string
+	Commit   Commit
+	Pipeline Pipeline
+}
+
+type GitlabProject struct {
+	GroupID string
+	ID      string
+}
+
+type Commit struct {
+	// Hash is a 20-byte SHA-1 encoded in hex.
+	Hash    string
+	Message string
+	Author  string
+}
+
+type PipelineStatus string
+
+const (
+	PipelineStatusOK     PipelineStatus = "ok"
+	PipelineStatusFailed PipelineStatus = "failed"
+)
+
+type Pipeline struct {
+	Status      PipelineStatus
+	ID          int64
+	TriggeredBy string
+	FailedJobs  []Job
+}
+
+type Job struct {
+	ID        int64
+	Name      string
+	Stage     string
+	RunnerLog string
+}
