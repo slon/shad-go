3758b2c27d705056b1fa8d25148353540d7851d9
Arseny Balobanov <verytable@yandex.ru>
2021-04-01 00:19:42 +0300

    [foolsday2] Fix test.

--- foolsday2/flag_test.go
+++ foolsday2/flag_test.go
@@ -2,10 +2,14 @@
 
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
 const flag = "FLAG{foolsday2:hidden-gem:07a5e6469f2178616cba4e9a0410e050}"
 
 func TestHiddenGem(t *testing.T) {
-	t.Logf("Here's your flag: %s", flag)
+	fmt.Fprintf(os.Stderr, "Here's your flag: %s", flag)
 }
