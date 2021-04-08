dab4076625e8e447dc6e6dd1576b0f0e8447530e
Ilya Sinelnikov <sidh@yandex-team.ru>
2021-04-08 16:41:48 +0300

    More database/sql fixes

--- lectures/07-sql/alive/ping.go
+++ lectures/07-sql/alive/ping.go
@@ -8,14 +8,14 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-func IsItAliveQuestionMark() {
+func IsItAliveQuestionMark(ctx context.Context) {
 	db, err := sql.Open("pgx", "postgres://pgx_md5:secret@localhost:5432/pgx_test")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	if err := db.PingContext(context.Background()); err != nil {
+	if err := db.PingContext(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
