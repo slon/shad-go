32f01223e44b24d99432095193cd4c62b8cc9478
Fedor Korotkiy <prime@yandex-team.ru>
2021-04-15 11:26:56 +0300

    Add blowfish task

---
+++ blowfish/blowfish.go
@@ -0,0 +1,14 @@
+// +build !solution
+
+package blowfish
+
+// #cgo LDFLAGS: -lcrypto
+// #include <openssl/blowfish.h>
+import "C"
+
+type Blowfish struct {
+}
+
+func New(key []byte) *Blowfish {
+	panic("implement me")
+}
