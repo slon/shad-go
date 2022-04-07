ddf9df28dde079f2117dd975a00a8b1739634c83
Arseny Balobanov <verytable@yandex.ru>
2022-04-07 18:04:16 +0300

    [lectures] Update generics slides.

---
+++ lectures/08-generics/scale/fixed/scale.go
@@ -0,0 +1,11 @@
+package wrong
+
+import "golang.org/x/exp/constraints"
+
+func Scale[S ~[]E, E constraints.Integer](s S, c E) S {
+	r := make(S, len(s))
+	for i, v := range s {
+		r[i] = v * c
+	}
+	return r
+}
---
+++ lectures/08-generics/scale/wrong/scale.go
@@ -0,0 +1,11 @@
+package wrong
+
+import "golang.org/x/exp/constraints"
+
+func Scale[E constraints.Integer](s []E, c E) []E {
+	r := make([]E, len(s))
+	for i, v := range s {
+		r[i] = v * c
+	}
+	return r
+}
