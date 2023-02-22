9ef98a6a0093b4f54b04406c05c87c33da930e7b
Fedor Korotkiy <dartslon@gmail.com>
2023-02-22 17:06:08 +0400

    Remove complex coverage tests

--- tools/testtool/testdata/submissions/incorrect/skippedcoverpkg/private/skippedcoverpkg/sum/sum.go
+++
@@ -1,8 +0,0 @@
-//go:build !change
-// +build !change
-
-package sum
-
-func Sum(x, y int) int {
-	return x + y
-}
--- tools/testtool/testdata/submissions/incorrect/skippedcoverpkg/student/skippedcoverpkg/product/product.go
+++
@@ -1,8 +0,0 @@
-//go:build !change
-// +build !change
-
-package product
-
-func Product(x, y int) int {
-	return x * y
-}
