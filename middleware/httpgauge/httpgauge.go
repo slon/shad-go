001e5430fdaf253c91bbd8d9175f645e4fb27950
Fedor Korotkiy <dartslon@gmail.com>
2023-03-26 16:59:47 +0400

    Describe gauge ServeHTTP method

--- middleware/httpgauge/httpgauge.go
+++ middleware/httpgauge/httpgauge.go
@@ -14,6 +14,13 @@ func (g *Gauge) Snapshot() map[string]int {
 	panic("not implemented")
 }
 
+// ServeHTTP returns accumulated statistics in text format ordered by pattern.
+//
+// For example:
+//
+//	/a 10
+//	/b 5
+//	/c/{id} 7
 func (g *Gauge) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	panic("not implemented")
 }
