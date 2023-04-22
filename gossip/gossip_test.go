0635caea1e357850604c5d01f992cab23b3fc98e
Fedor Korotkiy <dartslon@gmail.com>
2023-04-22 13:18:36 +0400

    Try fixing flaky test

--- gossip/gossip_test.go
+++ gossip/gossip_test.go
@@ -15,8 +15,8 @@ import (
 )
 
 const (
-	pingPeriod = time.Millisecond * 5
-	waitPeriod = pingPeriod * 20
+	pingPeriod = time.Millisecond * 50
+	waitPeriod = pingPeriod * 10
 )
 
 type env struct {
