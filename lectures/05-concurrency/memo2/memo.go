739c7a132688f4bdf1405c1ee56bebdf2077d517
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-03-19 22:57:18 +0300

    add one more defer

--- lectures/05-concurrency/memo2/memo.go
+++ lectures/05-concurrency/memo2/memo.go
@@ -32,12 +32,12 @@ type Memo struct {
 // Get is concurrency-safe.
 func (memo *Memo) Get(key string) (value interface{}, err error) {
 	memo.mu.Lock()
+	defer memo.mu.Unlock()
 	res, ok := memo.cache[key]
 	if !ok {
 		res.value, res.err = memo.f(key)
 		memo.cache[key] = res
 	}
-	memo.mu.Unlock()
 	return res.value, res.err
 }
 
