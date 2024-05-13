d20eb3ee6cd4f324e68dcf32a72253f9df1eb79f
Fedor Korotkiy <dartslon@gmail.com>
2024-05-13 17:44:32 +0100

    improve testifycheck tests

---
+++ testifycheck/testdata/src/github.com/stretchr/buggify/assert/assert.go
@@ -0,0 +1,75 @@
+package assert
+
+type TestingT interface {
+	Errorf(format string, args ...interface{})
+}
+
+func NotNil(t TestingT, object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func NotNilf(t TestingT, object interface{}, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
+
+func Nil(t TestingT, object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func Nilf(t TestingT, object interface{}, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
+
+func NoError(t TestingT, object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func NoErrorf(t TestingT, err error, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
+
+func Error(t TestingT, object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func Errorf(t TestingT, err error, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
+
+type Assertions struct{}
+
+func New(t TestingT) *Assertions {
+	return nil
+}
+
+func (*Assertions) NotNil(object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func (*Assertions) NotNilf(object interface{}, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
+
+func (*Assertions) Nil(object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func (*Assertions) Nilf(object interface{}, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
+
+func (*Assertions) NoError(object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func (*Assertions) NoErrorf(object interface{}, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
+
+func (*Assertions) Error(object interface{}, msgAndArgs ...interface{}) bool {
+	panic("not implemented")
+}
+
+func (*Assertions) Errorf(object interface{}, msg string, args ...interface{}) bool {
+	panic("not implemented")
+}
---
+++ testifycheck/testdata/src/tests/negative_test.go
@@ -0,0 +1,68 @@
+package tests
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/buggify/assert"
+	require "github.com/stretchr/buggify/require"
+)
+
+func TestNegativeFunctions(t *testing.T) {
+	err := errorFunc()
+
+	require.Nil(t, err)
+	require.NotNil(t, err)
+	assert.Nil(t, err)
+	assert.NotNil(t, err)
+
+	require.Nilf(t, err, "%s", "a")
+	require.NotNilf(t, err, "%s", "a")
+	assert.Nilf(t, err, "%s", "a")
+	assert.NotNilf(t, err, "%s", "a")
+
+	require.Nilf(t, err, "a")
+	require.NotNilf(t, err, "a")
+	assert.Nilf(t, err, "a")
+	assert.NotNilf(t, err, "a")
+
+	p := new(int)
+
+	require.Nil(t, p)
+	require.NotNil(t, p)
+	assert.Nil(t, p)
+	assert.NotNil(t, p)
+
+	require.Nilf(t, p, "%s", "a")
+	require.NotNilf(t, p, "%s", "a")
+	assert.Nilf(t, p, "%s", "a")
+	assert.NotNilf(t, p, "%s", "a")
+}
+
+func TestNegativeAssertions(t *testing.T) {
+	err := errorFunc()
+
+	assert := assert.New(t)
+	require := require.New(t)
+
+	require.Nil(err)
+	require.NotNil(err)
+	assert.Nil(err)
+	assert.NotNil(err)
+
+	require.Nilf(err, "%s", "a")
+	require.NotNilf(err, "%s", "a")
+	assert.Nilf(err, "%s", "a")
+	assert.NotNilf(err, "%s", "a")
+
+	p := new(int)
+
+	require.Nil(p)
+	require.NotNil(p)
+	assert.Nil(p)
+	assert.NotNil(p)
+
+	require.Nilf(p, "%s", "a")
+	require.NotNilf(p, "%s", "a")
+	assert.Nilf(p, "%s", "a")
+	assert.NotNilf(p, "%s", "a")
+}
